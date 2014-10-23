da0e25e6864c08cefcf92b31006206718ea713fd
Matthew Alan Gray <mgray@fncinc.com>
2014-10-23 15:15:42 -0500

    Refactoring CMake files to download and build boost dependencies as an external project.

--- CMake/Modules/Packaging.cmake
+++ CMake/Modules/Packaging.cmake
@@ -7,12 +7,12 @@ set ( CPACK_INSTALL_CMAKE_PROJECTS "${GCJFramework_BINARY_DIR}" "GCJFramework" "
 set ( CPACK_PACKAGE_DESCRIPTION_SUMMARY "Google Code Jam Solution Framework" )
 set ( CPACK_PACKAGE_INSTALL_DIRECTORY "GCJFramework" )
 set ( CPACK_PACKAGE_NAME "GCJFramework" )
-set ( CPACK_PACKAGE_VENDOR "hatboystudios.com" )
+set ( CPACK_PACKAGE_VENDOR "matthewalangray.com" )
 
 set ( CPACK_RESOURCE_FILE_LICENSE "${GCJ_PARENT}/GCJFramework/license.txt" )
 set ( CPACK_PACKAGE_ICON "${GCJ_PARENT}/GCJFramework/Setup/Package/gcjframework.ico" )
 
-set ( CPACK_PACKAGE_CONTACT "Matthew Alan Gray <mgray@hatboystudios.com>" )
+set ( CPACK_PACKAGE_CONTACT "Matthew Alan Gray <matthew.alan.gray@gmail.com>" )
 
 set ( CPACK_NSIS_EXTRA_INSTALL_COMMANDS "WriteRegStr \\\${WriteEnvStr_RegKey} \\\"GCJFramework_HOME\\\" $INSTDIR" )
 
